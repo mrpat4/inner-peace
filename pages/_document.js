46f874486b912c2a6bde2a66ab7dd4729bb62fe3
PrhmPym <mr.pat4@gmail.com>
2022-01-15 16:59:27 +0330

    FIX:fix light and dark mode

--- pages/_document.js
+++ pages/_document.js
@@ -39,6 +39,7 @@ export default class MyDocument extends Document {
           />
         </Head>
         <body>
+          <script src="noflash.js" />
           <Main />
           <NextScript />
         </body>
