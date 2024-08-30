62d81ab808c93bd00fa18fb9d8773cf231e86486
papa <mr.pat4@gmail.com>
2024-08-30 23:34:35 +0330

    EDIT:edit name

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -18,7 +18,11 @@ function About({ clickedNavState, setClickedNavState }) {
       <s.Left_side>
         <s.Title_wrapper>
           <h1 className="title">About Me</h1>
-          <a href="/image/Portfolio.pdf" download={"resume"} className={"sk-btn sk-btn gsapAnimate"}>
+          <a
+            href="/image/Portfolio.pdf"
+            download={"resume"}
+            className={"sk-btn sk-btn gsapAnimate"}
+          >
             <span className="sk-btn_wrapper">
               <span data-text="Download">My Resume</span>
             </span>
@@ -26,18 +30,22 @@ function About({ clickedNavState, setClickedNavState }) {
         </s.Title_wrapper>
         <div className="desc-wrapper description">
           <p className="">
-            I am Abbas (Parham) Payam, a seasoned Front End Developer with a strong focus on React and Next.js technologies. Over an impressive career
-            spanning over 4 years, I have honed my skills and expertise in the field of front-end development, becoming a reliable and innovative
-            professional in the process. I have had the privilege of working for 3 international companies, where I have been instrumental in driving
-            their digital transformation efforts. My contributions have ranged from developing user-friendly interfaces to optimizing web performance,
-            ensuring a seamless user experience across various platforms. My extensive experience, coupled with my passion for staying updated with
-            the latest industry trends, makes me an invaluable asset to any team. I am not just a developer; I am a problem solver who is always ready
-            to take on new challenges and deliver efficient solutions.
+            I am Parham Payam, a seasoned Front End Developer with a strong focus on React and
+            Next.js technologies. Over an impressive career spanning over 4 years, I have honed my
+            skills and expertise in the field of front-end development, becoming a reliable and
+            innovative professional in the process. I have had the privilege of working for 3
+            international companies, where I have been instrumental in driving their digital
+            transformation efforts. My contributions have ranged from developing user-friendly
+            interfaces to optimizing web performance, ensuring a seamless user experience across
+            various platforms. My extensive experience, coupled with my passion for staying updated
+            with the latest industry trends, makes me an invaluable asset to any team. I am not just
+            a developer; I am a problem solver who is always ready to take on new challenges and
+            deliver efficient solutions.
           </p>
           <h3>Tools i use :</h3>
           <p>
-            Next.js, React.js,Styled components, Tailwind css,Framer motion,Redux, Saga, TypeScript, axios, react-query, hook-form, socket io, yup and
-            ...
+            Next.js, React.js,Styled components, Tailwind css,Framer motion,Redux, Saga, TypeScript,
+            axios, react-query, hook-form, socket io, yup and ...
           </p>
           {/* <h3>Learning :</h3>
           <p>Currently im learning Three.js</p> */}
