8cf76242029fda00e7326da64508c6208f11a917
papa <mr.pat4@gmail.com>
2024-09-03 21:02:42 +0330

    ADD:add arash website,delete old panels

--- src/components/workComponents/seed.js
+++ src/components/workComponents/seed.js
@@ -1,31 +1,29 @@
-import albaAddCategory from "images/alba-add-category.png";
-import albaCategory from "images/alba-category.png";
-import albaDashboard from "images/alba-dashboard.png";
-import albaAddUser from "images/alba-users.png";
-import arash1 from "images/arash1.png";
-import arash2 from "images/arash2.png";
-import arash3 from "images/arash3.png";
-import arash4 from "images/arash4.png";
+import photographer from "images/arash.png";
+import fitgoApp from "images/fitgo.png";
+import kirtoglum from "images/kirtiloglutarim.png";
 import trader01 from "images/trader01.png";
 import trader02 from "images/trader02.png";
 import trader03 from "images/trader03.png";
 import trader04 from "images/trader04.png";
 import trader05 from "images/trader05.png";
+import villa from "images/villa.png";
 import wild6 from "images/widl6.png";
 import wild1 from "images/wild1.png";
 import wild2 from "images/wild2.png";
 import wild3 from "images/wild3.png";
 import wild4 from "images/wild4.png";
 import wild5 from "images/wild5.png";
-import wingleDashboard from "images/wingle-dashboard.png";
-import wingleLogin from "images/wingle-login.png";
-import wingleUserDashboard from "images/wingle-user-dashboard.png";
-import wingleUserList from "images/wingle-user-list.png";
-import wingleUser from "images/wingle-user.png";
-import fitgoApp from "images/fitgo.png";
-import kirtoglum from "images/kirtiloglutarim.png";
-import villa from "images/villa.png";
 export const workData = [
+  {
+    name: "Photographer",
+    description:
+      "arash is a filmmaker and photographer with over eight years of experience. His work has been recognized at various film festivals and photography contests.",
+    disabled: false,
+    images: [photographer],
+    link: "https://www.arash-payam.ir/",
+    showModal: false,
+  },
+
   {
     name: "haze website",
     description:
@@ -33,7 +31,7 @@ export const workData = [
     disabled: false,
     images: [wild1, wild2, wild3, wild4, wild5, wild6],
     link: "https://wild-kappa.vercel.app/",
-    main: "https://wild.as/",
+    // main: "https://wild.as/",
   },
   {
     name: "Villa Website",
@@ -75,22 +73,22 @@ export const workData = [
   //   disabled: false,
   //   link: "https://arash-payam.ir/",
   // },
-  {
-    name: "wingle panel",
-    description:
-      "This is a comprehensive Admin dashboard for the Wingle application, a social media app. We’ve utilized React, Redux, Saga, and Yup for validations, along with Axios for data fetching. The panel includes a custom table component and uses react-hook-form for form handling. Due to its extensive and detailed nature, I wish you could experience it firsthand. However, as it’s developed for a client, I’m unable to provide a link here. My apologies for the inconvenience.",
-    disabled: true,
-    images: [wingleLogin, wingleDashboard, wingleUserDashboard, wingleUser, wingleUserList],
-    link: "#",
-    showModal: true,
-  },
-  {
-    name: "alba education",
-    description:
-      "Alba Education is an admin dashboard designed for an educational app, which is used to input data into the application. The panel was developed using React, Redux, Saga, Axios, Yup, and react-hook-form. It’s fully functional and is used daily by the owner. Unfortunately, as it’s a private dashboard, I’m unable to provide a link. However, you can view the attached photos for a glimpse into its design and functionality.",
-    disabled: true,
-    images: [albaDashboard, albaCategory, albaAddCategory, albaAddUser],
-    link: "#",
-    showModal: true,
-  },
+  // {
+  //   name: "wingle panel",
+  //   description:
+  //     "This is a comprehensive Admin dashboard for the Wingle application, a social media app. We’ve utilized React, Redux, Saga, and Yup for validations, along with Axios for data fetching. The panel includes a custom table component and uses react-hook-form for form handling. Due to its extensive and detailed nature, I wish you could experience it firsthand. However, as it’s developed for a client, I’m unable to provide a link here. My apologies for the inconvenience.",
+  //   disabled: true,
+  //   images: [wingleLogin, wingleDashboard, wingleUserDashboard, wingleUser, wingleUserList],
+  //   link: "#",
+  //   showModal: true,
+  // },
+  // {
+  //   name: "alba education",
+  //   description:
+  //     "Alba Education is an admin dashboard designed for an educational app, which is used to input data into the application. The panel was developed using React, Redux, Saga, Axios, Yup, and react-hook-form. It’s fully functional and is used daily by the owner. Unfortunately, as it’s a private dashboard, I’m unable to provide a link. However, you can view the attached photos for a glimpse into its design and functionality.",
+  //   disabled: true,
+  //   images: [albaDashboard, albaCategory, albaAddCategory, albaAddUser],
+  //   link: "#",
+  //   showModal: true,
+  // },
 ];
